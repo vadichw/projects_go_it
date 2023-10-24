f2bc278e144d8b8ced05d3ff1277a6bc66ec4b92
vadichw <vadik56780098@gmail.com>
2023-10-24 13:31:10 +0300

    Solve 3.4

--- modul_3/task_3_4.py
+++ modul_3/task_3_4.py
@@ -1,9 +1,11 @@
 # Необхідно реалізувати функцію розрахунку ціни товару зі знижкою discount_price. Функція приймає ціну price та знижку discount — це дрібне число від 0 до 1. Тут і надалі ми під знижкою розумітимемо коефіцієнт, який визначає розмір від ціни. І на цей розмір ми знижуємо підсумкову вартість товару. Логіку функції необхідно прописати у внутрішній функції apply_discount, використовуючи оголошення зміною price як nonlocal.
 
 def discount_price(price, discount):
-    
     def apply_discount():
-        nonlocal price
-        
+       nonlocal price
+       price = price - (price * discount)
     apply_discount()
     return price
+
+finally_price = discount_price(100, 0.1)
+print(f"The price with discount is {finally_price}")
