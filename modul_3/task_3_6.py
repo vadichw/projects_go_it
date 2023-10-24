399c17106b11c7ff546b649cb158d7280b5fb2bb
vadichw <vadik56780098@gmail.com>
2023-10-24 16:43:03 +0300

    Ad

--- modul_3/task_3_6.py
+++ modul_3/task_3_6.py
@@ -2,10 +2,11 @@
 #Якщо довжина вихідного рядка більша або дорівнює length, ми повертаємо його в тому ж вигляді;
 #Якщо вона менша length, ми додаємо попереду рядка пробіли в кількості (length - len(string)) // 2.
 
-def format_string(string, length):
+def format_string(length=15, string='abaa'):
     if len(string) >= length:
         return string
     else:
         spaces = (length - len(string)) // 2
         return ((' ' * spaces) + string)
-print(format_string('vadim' , 50))
+    
+print(format_string())
