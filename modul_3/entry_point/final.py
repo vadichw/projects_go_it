2aeca05da48d429a905fbed258dda57e09bd9b7a
vadichw <vadik56780098@gmail.com>
2023-10-28 11:28:30 +0300

    Add entry point

---
+++ modul_3/entry_point/final.py
@@ -0,0 +1,10 @@
+from main import get_name
+
+def goodbuy(name):
+    print(f"Goodbuy {name}")
+
+def main():
+    name = get_name()
+    goodbuy(name)
+    
+main()
--- modul_3/entry_point/main.py
+++ modul_3/entry_point/main.py
@@ -1,8 +1,15 @@
-def get_name(name):
+def get_name():
     name = input("Enter name ")
     return name
+
 def greet(name):
     print(f"Hello, my Lord {name}")
+
 def main():
     name = get_name()
     greet(name)
+
+# ENTRY POINT 
+# ЧТОБ В 2ом файле НЕ ВЫЗЫВАЛСЯ ФУНКЦИОНАЛ 1го
+if __name__ == "__main__":
+    main()
