1af4d73dc61987580fafc81d8a638912c66eb466
vadichw <vadik56780098@gmail.com>
2023-10-30 13:17:46 +0200

    Add

--- modul_4/task_4_3.py
+++ modul_4/task_4_3.py
@@ -3,11 +3,11 @@ def format_ingredients(items):
         return ''
     if len(items) == 1:
         return items[0]
-            #results
     # result = ', '.join(map(str, items[:-1]))
     result = f"{', '.join(items[:-1])} and {items[-1]}"
     # result += f' and {str(items[-1])}'
     return result
+
 print(format_ingredients(["2 eggs", "1 liter sugar", "1 tsp salt", "vinegar"]))
 
 print(format_ingredients(["2 eggs"]))
