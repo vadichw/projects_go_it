a06d25ec4ceeae9cba1f26181d618279cfa94ec4
vadichw <vadik56780098@gmail.com>
2023-10-30 18:41:10 +0200

    add

--- modul_4/task_4_5.py
+++ modul_4/task_4_5.py
@@ -1,8 +1,9 @@
-data = {'key1': 1, 'key2': 2, 'key3': 3, 'key4': 2}
 def lookup_key(data, value):
+    data = {'key1': 1, 'key2': 2, 'key3': 3, 'key4': 2}
     keys = []
     for key, val in data.items():
         if val == value:
             keys.append(key)
     return keys
+print(lookup_key('', 2))
 
