00af50a9e96714fc6ab6451709c2ef80c48dbe27
vadichw <vadik56780098@gmail.com>
2023-11-04 12:01:57 +0200

    add task_4_9

--- modul_4/task_4_9.py
+++ modul_4/task_4_9.py
@@ -4,6 +4,15 @@ def is_valid_pin_codes(pin_codes):
 
     list = set(pin_codes)
 
+# Перевірка на дублікати та валідність пін-кодів
+    if len(list) != len(pin_codes):
+        return False
+    for pin_codes in list:
+        if len(pin_codes) != 4 or not pin_codes.isdigit():
+            return False
+
+    return True
+    
 
     
 
