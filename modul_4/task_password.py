c49a58260720d662dc2d99f4dabec8303296579f
vadichw <vadik56780098@gmail.com>
2023-11-05 14:32:01 +0200

    add

--- modul_4/task_password.py
+++ modul_4/task_password.py
@@ -2,9 +2,8 @@ from random import randint
 
 def get_random_password():
     password = ""
-
     for i in range(8):
-        random_char = chr(randint(40,126))
+        random_char = chr(randint(40, 126))
         password += random_char
     return password
 
@@ -25,20 +24,18 @@ def is_valid_password(password):
             small = True
         elif ch.isdigit():
             num = True
-    #big = any(i.isupper() for i in password)
-    #small = any(i.islower() for i in password)
-    #num = any(i.isdigit() for i in password)
-
     return big and small and num
-
+    
 def get_password():
-    max = 100
+    max_attempts = 100
     attempts = 0
-    while attempts < max:
+    while attempts < max_attempts:
         password = get_random_password()
         if is_valid_password(password):
             return password
         attempts += 1
+    
 
 print(get_password())
+
 
