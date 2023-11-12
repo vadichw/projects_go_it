e699af6abaf65f96ecdb8c00044c44842d5cd867
vadichw <vadik56780098@gmail.com>
2023-11-12 10:16:43 +0200

    new

--- modul_5/task_5_10.py
+++ modul_5/task_5_10.py
@@ -1,20 +1,3 @@
-# import re
-# from pprint import pprint
-
-# def find_word(text, word):
-#     search_result = text.find(word)
-#     result_dict = {
-#         'result': search_result != -1,
-#         'first_index': search_result if search_result != -1 else None,
-#         'last_index': search_result + len(word) - 1 if search_result != -1 else None,
-#         'search_string': word,
-#         'string': text
-#     }
-#     return result_dict
-
-#pprint(find_word( "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.", "Python"))
-
-
 import re
 from pprint import pprint
 
