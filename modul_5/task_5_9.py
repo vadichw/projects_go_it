1a8cfc392cfc2a719632c032430d7c41360a5e1b
vadichw <vadik56780098@gmail.com>
2023-11-12 09:09:39 +0200

    add new

--- modul_5/task_5_9.py
+++ modul_5/task_5_9.py
@@ -1,24 +1,37 @@
 def formatted_numbers():
-    table = []
+    num_list = ["|{:^10}|{:^10}|{:^10}|".format("decimal", "hex", "binary")]
+    for num in range(16):
+        num_list.append("|{:<10d}|{:^10x}|{:>10b}|".format(num, num, num))
+    return num_list
+    
+
+
+
+
+
+#def formatted_numbers():
+    #table = []
 
     # Header
-    header = "| {:<10} | {:^10} | {:>10} |".format("decimal", "hex", "binary")
-    table.append(header)
+    #header = "| {:<10} | {:^10} | {:>10} |".format("decimal", "hex", "binary")
+    #table.append(header)
     
     #Separator
-    separator = "-" * len(header)
-    table.append(separator)
+    #separator = "-" * len(header)
+    #table.append(separator)
 
     # Data rows
-    for i in range(16):
-        decimal = "{0:<10d}".format(i)
-        hex = "{0:^10o}".format(i)
-        binary = "{0:>10b}".format(i)
+    #for i in range(16):
+        #decimal = "{0:<10d}".format(i)
+        #hex = "{0:^10o}".format(i)
+        #binary = "{0:>10b}".format(i)
         ###
-        line = "| {} | {} | {} |".format(decimal, hex, binary)
-        table.append(line)
+        #line = "| {} | {} | {} |".format(decimal, hex, binary)
+        #table.append(line)
 
-    return table
+    #return table
 
-for el in formatted_numbers():
-    print(el)
+#for el in formatted_numbers():
+    #print(el)
+
+###
