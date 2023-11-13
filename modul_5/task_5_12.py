bf9b8bff680663cebfe6442958d3b2f2f5e23b30
vadichw <vadik56780098@gmail.com>
2023-11-13 09:01:28 +0200

    add

--- modul_5/task_5_12.py
+++ modul_5/task_5_12.py
@@ -1,8 +1,8 @@
 import re
 
 def replace_spam_words(text, spam_words):
-    for word in spam_words:
+    for bad_word in spam_words:
         pattern = re.compile(r'\b{}\b'.format(re.escape(word)), re.IGNORECASE)
-        text = pattern.sub('*' * len(word), text)
+        text = pattern.sub('*' * len(bad_word), text)
 
     return text
